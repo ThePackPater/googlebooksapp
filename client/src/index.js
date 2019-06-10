1497a27d0b7f98d1a660449c65d24ea92a84cc7f
Andy Pack <thepackpater@gmail.com>
2019-06-10 09:47:03 -0500

    updated file structure added App.js and Server.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider as AlertProvider} from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Alert Default Options
+const options = {
+  position: 'top center',
+  timeout: 3000,
+  offset: '30px',
+  transition: 'scale'
+};
+
+// Wrapping the Application in the Alert Provider
+class Root extends Component {
+  render () {
+    return (
+      <AlertProvider template={AlertTemplate} {...options}>
+        <App/>
+      </AlertProvider>
+    )
+  }
+}
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
