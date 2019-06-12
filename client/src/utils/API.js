b6c4340443830074abb98079642680aa60586c7d
Andy Pack <thepackpater@gmail.com>
2019-06-12 18:18:55 -0500

    updated controller, APi

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,6 +6,7 @@ const PATH_BASE = 'https://www.googleapis.com/books/v1';
 const PATH_SEARCH = '/volumes';
 const PARAM_SEARCH = 'q=';
 const PARAM_HPP = 'maxResults=';
+// hide key in file 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default {
